d8f59bf39da04064fa7ba3a861ef89600e2dd312
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-02-11 21:15:53 +0500

    Create seccondclass.py

---
+++ Task#2/seccondclass.py
@@ -0,0 +1,12 @@
+fromcreeps = ('Ali' 'Asad', 'Farah', 'Shameer', 'Subhan', 'Salar', 'Hafsa', 'Sana', 'Hoora', 'Ahad', 'Saif', 'Maryam', 'Irfan', 'Furqan', 'Mehroz')
+print(type(fromcreeps)) 
+
+#i choosed tuple because employer are permanent and will not be change
+
+gluty = {'Orange', 'pineapple', 'grapes', 'tutyfruty', 'pepsi,' 'pakola', 'reditrish'}
+print(type(gluty))
+#I choosed set because flavours are changing but they will not add duplicate flavours
+
+pharma = {'Ventilator', 'CTscan' , 'Defibrillator', 'ECG machine', 'Patient monitor', 'Syringes', 'MRI'}
+print(type(pharma))
+# i choosed set becuase they already have Patient monitor and Defibrillator so they will not sell product of other brand
