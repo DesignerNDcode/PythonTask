01b99d6624c93e53b4f2c424707ed5a5f5122e5e
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-02-15 19:46:35 +0500

    Create Day5-Task.py

---
+++ Task#3/Day5-Task.py
@@ -0,0 +1,64 @@
+#Task1
+print('Task 1.................')
+print('Welcome To Zebtech')
+name = input('Please enter your name')
+print('Great!', name)
+
+height = float(input('Please tell your height'))
+print(height)
+
+contact = int(input('Please enter your contact number'))
+print(contact)
+print('Great job! form submitted')
+
+#Task1
+
+#Task2
+print('Task 2.................')
+print('Welcome To Skillvention')
+print('Answer some questions below to submit form')
+
+name = input('What is your name')
+print( 'Good', name)
+
+fathername = input('what is your father/gaurdian name?')
+print(fathername)
+
+email = input('what is you email address?')
+print(email)
+
+contact = int(input('what is your contact number?'))
+print(contact)
+
+skill = input('Do you already have any skill? Yes/no')
+print(skill)
+
+#Task2
+
+#Task3
+print('Task 3.................')
+print('welcome to Python Agency')
+print('The real world of tech')
+
+print('please register')
+
+Name = input('What is your name?')
+print(Name)
+
+Email = input('Enter you email id')
+print('Email')
+
+password = input('Enter your password')
+print(password)
+
+number = int(input('Enter your number'))
+print(number)
+print('Great now login..........')
+Email = input('Enter you email id')
+print('Email')
+
+password = input('Enter your password')
+print(password)
+print('successfully loggedin')
+
+#Task3
