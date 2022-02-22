6f00261a3c2704baf8cad2d87aa59bb478d5e460
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-02-22 19:41:44 +0500

    Create task5.py

---
+++ Task#5/task5.py
@@ -0,0 +1,57 @@
+# -----------Task1-----------
+print('-----------Task1-----------')
+print('Hello Welcome to CalcPY')
+a = int(input('Enter first digit'))
+b = int(input('Enter seccond digit'))
+print(a + b, "Added")
+print(a - b, "substracted")
+print(a / b, "Divided")
+print(a * b, "multiplied")
+print(a // b, "floor divided")
+
+
+# -----------Task2-----------
+print('-----------Task2-----------')
+first = int(input('Enter first argument'))
+seccond = int(input('Enter seccond argument'))
+print(first == seccond)
+
+# -----------Task3-----------
+print('-----------Task3-----------')
+print('Welcome to Area Calculator')
+width = float(input("enter the width"))
+length = float(input("enter the length"))
+print("Your plot total are is", width * length)
+
+# -----------Task4-----------
+print('-----------Task4-----------')
+print("BMI calc in metrics ")
+weight = float(input("enter weight in kilograms"))
+height = float(input("enter height in cm"))
+
+print( weight / (height/100)**2 , "Answer in metric")
+
+print("BMI calc in US------- ")
+
+
+
+print("BMI calc in us ")
+# could not find
+
+
+# -----------Task5-----------
+print('-----------Task5-----------')
+print('Welcome to Converting Calculator')
+# User weight in kG into pounds
+weight = float(input("Enter your weight in kg"))
+print("your weight in pounds is", weight * 2.2)
+
+print('-----------Task6-----------')
+print('calculate area of trapezium')
+# could not find
+
+
+
+
+
+
