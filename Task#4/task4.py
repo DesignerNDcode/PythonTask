d1b563e693e62bf11188d3208347085cbfc7e12e
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-02-27 23:05:30 +0500

    Update task4.py

--- Task#4/task4.py
+++ Task#4/task4.py
@@ -1,1 +1,16 @@
-# have to complete
+print('--------Task 1--------')
+email = input('Enter your email address: ')
+print(email.endswith('.com'))
+
+print('===========Task 2===========')
+name = input('Please tell me your name: ')
+print('Hello ', name.title(), ', I hope you are fine. please let me know how can u help you?')
+user = input(' Do you need any help? ')
+print('okay, Get in touch')
+
+print('==============task 3=============')
+
+url = input('Enter your url: ')
+print(url.startswith('https://www.'))
+
+print(url.replace('https://www.', name))
