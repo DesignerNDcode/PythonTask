39de01bdcff3e0e26135ca6c4f05c999545ca112
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-04-02 23:15:37 +0500

    Create task10.3.py

---
+++ Task# 10/task10.3.py
@@ -0,0 +1,72 @@
+# Year 2023
+print("         January          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        February          ")
+for d in range(1, 29):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        March          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        April          ")
+for d in range(1, 30):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        May          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        June          ")
+for d in range(1, 31):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        July          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        August          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        September          ")
+for d in range(1, 31):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        October          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        November          ")
+for d in range(1, 31):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
+
+print("\n", "        December          ")
+for d in range(1, 32):
+    print("{:3}".format(d), end="")
+    if d % 7 == 0:
+        print()
