be3329031e5ce42a73a7d33595b4e5f7b3ec89be
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-04-02 22:25:41 +0500

    Create Task#13 (1).py

---
+++ Task#13/Task#13 (1).py
@@ -0,0 +1,26 @@
+import string
+import random
+# Task 1.1 ============================
+print("Task 1.1 ============================")
+
+Hex = ["a", "b", "c", "d", "f", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print("#", random.choices(Hex, k=6))
+# Task 1.1 ============================
+
+# Task 1.2 ============================
+print("Task 1.2 ============================")
+a = string.ascii_lowercase
+b = random.choices(a, k=26)
+for i in b:
+    print(i, end=" ")
+# Task 1.2 ============================
+print("\n")
+
+# Task 1.3 ============================
+print("Task 1.3 ============================")
+
+
+# Task 1.3 ============================
+j = random.randint(5, 10, )
+print(j)
+
