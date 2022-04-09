f05872ccf7087e2680d4139ead6b061d0929e453
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-04-09 16:38:31 +0500

    Update Task#1.py

--- TestTask/Task#1.py
+++ TestTask/Task#1.py
@@ -1,29 +1,7 @@
-a = [['happy', 'BirtHday', 'to', 'yoU'], ['this', 'Is', 'MY', 'BdaY', 'Month'], ['wish', 'me', 'HappY', 'BirtHday']]
+a = [['happy', 'BirtHday', 'to', 'yoU'], ['this', 'Is', 'MY', 'BdaY', 'Month'], ['wish', 'me', 'HappY', 'BirtHday'], ['i', 'did', 'it']]
 
-# List 1 of a
-print("Before:", (a[0]))
 
-for i in range(len(a[0])):
-    a[0][i] = a[0][i].upper()
-
-print("After:", (a[0]))
-
-print("=======================")
-
-# List 2 of a
-print("Before:", (a[1]))
-
-for i in range(len(a[1])):
-    a[1][i] = a[1][i].upper()
-
-print("After:", (a[1]))
-
-print("=======================")
-
-# List 3 of a
-print("Before:", (a[2]))
-
-for i in range(len(a[2])):
-    a[2][i] = a[2][i].upper()
-
-print("After:", (a[2]))
+for h in range(len(a)):
+    for i in range(len(a[h])):
+        a[h][i] = a[h][i].upper()
+print(a)
